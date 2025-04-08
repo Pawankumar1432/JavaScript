4f6dfac7916bb1afe2c2bf828fd9d6b998ccf4f0
Pavankumar <byallpawan@gmail.com>
2025-04-09 00:06:23 +0530

    first : commit

--- operators/index.js
+++ operators/index.js
@@ -1,4 +1,5 @@
 // Arithmetic Operators
+// +, -, *, /, %, **
 let a = 10, b = 5;
 console.log("Addition:", a + b);   // 15
 console.log("Subtraction:", a - b); // 5
@@ -6,7 +7,6 @@ console.log("Multiplication:", a * b); // 50
 console.log("Division:", a / b);   // 2
 console.log("Modulus:", a % b);   // 0
 console.log("Exponentiation:", a ** b); // 100000
-
 // Assignment Operators
 let x = 10;
 x += 5; // x = x + 5
